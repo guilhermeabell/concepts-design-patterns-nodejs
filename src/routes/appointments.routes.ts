035d48d5f256f4a660c12013748ca4a6d75a8cee
guilhermeabel99 <gpeuvis@gmail.com>
2021-01-02 17:06:34 -0300

    feature

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,22 +1,39 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
-const appointments = [];
+appointmentsRouter.get('/', (request, response) => {
+ const appointments = appointmentsRepository.all();
+ 
+ return response.json(appointments);
+});
+
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
-  const appointment = { 
-    id: uuid(),
-    provider,
-    date,
-  }
+  const parsedDate = startOfHour(parseISO(date));
 
-  appointments.push(appointment);
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
+    );
 
+    if (findAppointmentInSameDate) {
+      return response
+      .status(400)
+      .json({ message: 'This appointment is already booked '});
+    }
+
+    const appointment = appointmentsRepository.create({
+      provider,
+      date: parsedDate,
+    });
+    
   return response.json(appointment);
-})
+});
 
 export default appointmentsRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 import routes from './routes';
 
+
 const app = express();
 
+app.use(express.json());
 
-app.use('/', (request, response) => {
+app.use(routes);
+
+
+app.get('/', (request, response) => {
     return response.json({message: 'Hello World'})
 })
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log('Server started on port 3333!!!')
+});
